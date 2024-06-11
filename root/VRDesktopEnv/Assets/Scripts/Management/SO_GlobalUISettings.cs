63bc27a70108d5baec5a2a2901a75a5dd7f75ae9
Wyatt Murray <wyattmurray1235@gmail.com>
2024-06-11 01:34:30 -0400

    VR grid addition

--- root/VRDesktopEnv/Assets/Scripts/Management/SO_GlobalUISettings.cs
+++ root/VRDesktopEnv/Assets/Scripts/Management/SO_GlobalUISettings.cs
@@ -2,17 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SO_GlobalUISettings : MonoBehaviour
+[CreateAssetMenu(menuName = "GlobalUISettings")]
+public class SO_GlobalUISettings : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
--- root/VRDesktopEnv/Assets/Scripts/Objects/ScalableGrid.cs
+++ root/VRDesktopEnv/Assets/Scripts/Objects/ScalableGrid.cs
@@ -24,12 +24,12 @@ public class ScalableGrid : MonoBehaviour
         }
     }
 
-    private int TileXAmount
+    private int TileZAmount
     {
         get
         {
-            float val = CellSize.x + CellPadding.x;
-            return (int)(ColliderBounds.x / val);
+            float val = CellSize.z + CellPadding.z;
+            return (int)(ColliderBounds.z / val);
         }
     }
     private int TileYAmount
@@ -41,12 +41,12 @@ public class ScalableGrid : MonoBehaviour
         }
     }
 
-    public VRGridData Grid;
+    public VRGridData VRGrid;
 
     public void SetupGrid(int x, int y, GameObject[] objects)
     {
         if(x < 0 || y < 0) { return; }
-        Grid = new VRGridData(x, y, objects);
+        VRGrid = new VRGridData(x, y, objects);
     }
 
     public void SetGridItem(GameObject cell, Vector2Int location = default)
@@ -63,40 +63,97 @@ public class ScalableGrid : MonoBehaviour
             }
 
             //setup grid again
-            SetupGrid(TileXAmount, TileYAmount, children);
+            SetupGrid(TileZAmount, TileYAmount, children);
         }
         else
         {
-            Grid.tiles[location.x * location.y] = cell;
+            VRGrid.tiles[location.x * location.y] = cell;
         }
         
     }
 
     private void Awake()
     {
-        GameObject[] children = new GameObject[transform.childCount - 1];
-        for (int i = 0; i < transform.childCount - 1; i ++) 
+        GameObject[] children = new GameObject[transform.childCount];
+        for (int i = 0; i < transform.childCount; i ++) 
         {
             children[i] = transform.GetChild(i).gameObject;
         }
 
-        SetupGrid(TileXAmount, TileYAmount, children);
+        SetupGrid(TileZAmount, TileYAmount, children);
+
+        SetObjects();
     }
 
     public void Start()
     {
-        Debug.Log(ColliderBounds);
+
+    }
+
+    public void FixedUpdate()
+    {
+        //debugging
+        SetObjects();
     }
 
     private void SetObjects()
     {
-        //find center
-        //find offeset to start at
-        //extents minus padding?
+        // Find center of the collider
+        Vector3 center = ColliderCenter;
 
-        //didivde volume to find location for each cell
-        //place child cells at locations
+        // Get the extents (half size) of the collider
+        Vector3 extentsMin = GetComponent<BoxCollider>().bounds.min;
+        Debug.Log(extentsMin);
+
+        // Calculate starting area
+        Vector3 placementPosition = new Vector3(0f, extentsMin.y + CellPadding.y, extentsMin.z + (CellPadding.z * 2));
+
+        Vector3 sizeOfCell = CellSize + CellPadding;
+
+        int counter = 0;
+
+        // Loop through each cell position in the grid
+        for (int y = TileYAmount - 1; y >= 0 && counter < VRGrid.tiles.Length; y--)
+        {
+            for (int z = 0; z < TileZAmount && counter < VRGrid.tiles.Length; z++)
+            {
+                Vector3 setPlacement = new Vector3(0f, y * sizeOfCell.y, z * sizeOfCell.z);
+
+                // Place a child object at the calculated position
+                VRGrid.tiles[counter].transform.position = placementPosition + setPlacement;
+
+                //confirms if the objects can be enabled or disabled
+                if (!VRGrid.tiles[counter].gameObject.activeSelf && IsPositionWithinCollider(VRGrid.tiles[counter].transform.position))
+                {
+                    VRGrid.tiles[counter].gameObject.SetActive(true);
+                }
+
+                counter++;
+
+                // Break out of the loop if counter exceeds tile length
+                if (counter >= VRGrid.tiles.Length)
+                    break;
+            }
+        }
+
+        // Disable remaining objects
+        for (int i = counter; i < VRGrid.tiles.Length; i++)
+        {
+            VRGrid.tiles[i].gameObject.SetActive(false);
+        }
     }
+
+    private bool IsPositionWithinCollider(Vector3 position)
+    {
+        Collider collider = GetComponent<Collider>();
+        if (collider != null)
+        {
+            return collider.bounds.Contains(position);
+        }
+        return false;
+    }
+
+
 
     public struct VRGridData
     {
